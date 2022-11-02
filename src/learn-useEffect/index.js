8659b7e2af9127d3a2041678f5d701ffcf9c665f
Shaqq771 <63134150+Shaqq771@users.noreply.github.com>
2022-11-03 05:27:43 +0700

    Update Index.js di useEffect

--- src/learn-useEffect/index.js
+++ src/learn-useEffect/index.js
@@ -2,3 +2,31 @@ import React, { useState, useEffect } from "react";
 import "./index.css";
 import logo from "../logo.svg";
 
+export default function Index() {
+  let [dogImage, setDogImage] = useState([]);
+
+  useEffect(() => {
+    let mounting = true;
+    getAPI().then((data) => {
+      if (mounting) {
+        setDogImage(data.message);
+      }
+      return () => (mounting = false);
+    });
+  }, []);
+
+  return (
+    <div className="Main">
+        <p>Kelompok 06</p>
+      {dogImage &&
+        dogImage.map((dog) => (
+          <img width={"200px"} height={"200px"} src={dog}></img>
+        ))}
+    </div>
+  );
+}
+function getAPI() {
+  return fetch("https://dog.ceo/api/breeds/image/random/1").then((response) =>
+    response.json()
+  );
+}
